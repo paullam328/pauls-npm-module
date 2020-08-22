6e52b658e1fa623cc1dbcaa56b48c23c01c94687
Paul Lam <paullam@Pauls-MacBook-Pro.local>
2020-08-22 14:52:13 -0700

    Added datatable Component

--- index.js
+++ index.js
@@ -5,7 +5,79 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+var _react = _interopRequireDefault(require("react"));
+
+var _reactBootstrap = require("react-bootstrap");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var DataTable = /*#__PURE__*/function (_React$Component) {
+  _inherits(DataTable, _React$Component);
+
+  var _super = _createSuper(DataTable);
+
+  function DataTable() {
+    var _this;
+
+    _classCallCheck(this, DataTable);
+
+    _this.state = {
+      firstName: "Willy"
+    };
+    return _possibleConstructorReturn(_this);
+  }
+
+  _createClass(DataTable, [{
+    key: "render",
+    value: function render() {
+      /*#__PURE__*/
+      _react["default"].createElement(_reactBootstrap.Table, {
+        striped: true,
+        bordered: true,
+        hover: true,
+        variant: "dark"
+      }, /*#__PURE__*/_react["default"].createElement("thead", null, /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("th", null, "#"), /*#__PURE__*/_react["default"].createElement("th", null, "First Name"), /*#__PURE__*/_react["default"].createElement("th", null, "Last Name"), /*#__PURE__*/_react["default"].createElement("th", null, "Username"))), /*#__PURE__*/_react["default"].createElement("tbody", null, /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", null, "1"), /*#__PURE__*/_react["default"].createElement("td", null, this.state.firstName), /*#__PURE__*/_react["default"].createElement("td", null, "Otto"), /*#__PURE__*/_react["default"].createElement("td", null, "@mdo")), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", null, "2"), /*#__PURE__*/_react["default"].createElement("td", null, "Jacob"), /*#__PURE__*/_react["default"].createElement("td", null, "Thornton"), /*#__PURE__*/_react["default"].createElement("td", null, "@fat")), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", null, "3"), /*#__PURE__*/_react["default"].createElement("td", {
+        colSpan: "2"
+      }, "Larry the Bird"), /*#__PURE__*/_react["default"].createElement("td", null, "@twitter"))));
+    }
+  }]);
+
+  return DataTable;
+}(_react["default"].Component);
+
+exports["default"] = DataTable;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
 var _react = _interopRequireWildcard(require("react"));
+
+var _DataTable = require("./Table/DataTable");
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
@@ -47,7 +119,7 @@ var DummyComponent = /*#__PURE__*/function (_Component) {
   _createClass(DummyComponent, [{
     key: "render",
     value: function render() {
-      return /*#__PURE__*/_react["default"].createElement("div", null, " I am a dummy react npm module ");
+      return /*#__PURE__*/_react["default"].createElement(_DataTable.DataTable, null);
     }
   }]);
 
